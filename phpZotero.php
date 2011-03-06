a94a2cf6bee938660c167a0933ff7006613eeab0
Jeremy Boggs <jeremy@clioweb.org>
2011-03-05 19:44:43 -0500

    Adds phpDoc for main class.

--- phpZotero.php
+++ phpZotero.php
@@ -1,37 +1,16 @@
 <?php
-/***************************************************************/
-/* phpZotero - a library for accessing the Zotero.org API
-
-  Software License Agreement (BSD License)
-
-  Copyright (C) 2009-2011, Jeremy Boggs.
-  All rights reserved.
-  
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions are met:
-
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above copyright
-       notice, this list of conditions and the following disclaimer in the
-       documentation and/or other materials provided with the distribution.
-     * Neither the name of Jeremy Boggs nor the names of its contributors 
-       may be used to endorse or promote products derived from this software 
-       without specific prior written permission of Jeremy Boggs.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS "AS IS" AND ANY
-  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
-  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-                                                                 */
-/***************************************************************/
-
+/**
+ * @version $Id$
+ * @copyright Jeremy Boggs, 2009-2011
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @package phpZotero
+ */
+ 
+/**
+ * Primary class for using the Zotero API.
+ *
+ * @package phpZotero
+ */
 class phpZotero {    
     
     const ZOTERO_URI = 'https://api.zotero.org/';
