238d20db439e91e1a30e73d8a9e55cb6ebb37a50
Faolan C-P <fcheslack@gmail.com>
2011-04-21 11:39:21 -0400

    add mappings to maybe be used for cached zotero fields/localizations

---
+++ classes/Mappings.php
@@ -0,0 +1,13 @@
+<?php
+
+class Zotero_Mappings
+{
+    public $itemTypes = array();
+    public $itemFields = array();
+    public $itemTypeCreatorTypes = array();
+    public $creatorFields = array();
+    
+    
+}
+
+?>
