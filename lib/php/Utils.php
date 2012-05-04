e68e7f46f16bc23bce9458cb6d7f933df8b79b1a
Faolan C-P <fcheslack@gmail.com>
2012-05-04 00:44:44 -0400

    link wrapping that should probably never be used by anyone, but I'll just put it here anyway

---
+++ lib/php/Utils.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Utility functions for libZotero
+ * 
+ * @package libZotero
+ */
+class Zotero_Lib_Utils
+{
+    public static function wrapLinks($txt, $nofollow=false){
+        //extremely ugly wrapping of urls in html
+        if($nofollow){
+            $repstring = " <a rel='nofollow' href='$1'>$1</a>";
+        }
+        else{
+            $repstring = " <a href='$1'>$1</a>";
+        }
+        //will break completely on CDATA with unescaped brackets, and probably on alot of malformed html
+        return preg_replace('/(http:\/\/[-a-zA-Z0-9._~:\/?#\[\]@!$&\'\(\)*+,;=]+)(?![^<]*>)/i', $repstring, $txt);
+        
+        
+        //alternative regexes
+        /*
+        return preg_replace('/(?<!<[^>]*)(http:\/\/[\S]+)(?=\.|,|;)/i', " <a href='$1'>$1</a>", $txt);
+        return preg_replace('/<(?[^>]+>)(http:\/\/[\S]+)(?=\.|,|;)/i', " <a href='$1'>$1</a>", $txt);
+        
+        return preg_replace('/\s(http:\/\/[\S]+)(?=\.|,|;)/i', " <a href='$1'>$1</a>", $txt);
+        */
+    }
+    
+    public static function wrapDOIs($txt){
+        
+    }
+}
+
+
