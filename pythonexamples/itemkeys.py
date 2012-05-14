b392afe0d8de7ba7e70696be050c4eb697aada2f
Faolan C-P <fcheslack@gmail.com>
2012-05-14 13:59:22 -0400

    more example code

---
+++ pythonexamples/itemkeys.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('../lib/py/zotero')
+import json
+import zotero
+
+creds = json.loads(open('./user_writing_config.json').read())  # library credentials
+print(creds)
+zlib = zotero.Library(creds['libraryType'], creds['libraryID'], creds['librarySlug'], creds['apiKey'])
+
+itemKeys = zlib.fetchItemKeys()
+print("got " + len(itemKeys) + " item keys from Zotero api")
+
+for k in itemKeys:
+    print k
---
+++ pythonexamples/recentitems.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('../lib/py/zotero')
+import json
+import zotero
+
+creds = json.loads(open('./user_writing_config.json').read())  # library credentials
+print(creds)
+zlib = zotero.Library(creds['libraryType'], creds['libraryID'], creds['librarySlug'], creds['apiKey'])
+
+collectionKey = None
+items = zlib.fetchItems({'limit': 10, 'collectionKey': collectionKey, 'order': 'dateAdded', 'sort': 'desc'})
+
+for i in items:
+    print(i.title)
