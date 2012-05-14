a4f74f3dc840cbcb160fae39ba248d8eea947757
Faolan C-P <fcheslack@gmail.com>
2012-05-14 11:32:55 -0400

    python dicts are not php arrays

--- lib/py/zotero/entry.py
+++ lib/py/zotero/entry.py
@@ -3,6 +3,11 @@ import xml.dom.minidom
 
 class Entry(object):
     def __init__(self, entryNode):
+        if isinstance(entryNode, basestring):
+            entryNode = xml.dom.minidom.parseString(entryNode)
+            if not entryNode:
+                raise Exception("Error constructing feed Domdoc")
+
         self.title = entryNode.getElementsByTagName('title').item(0).childNodes.item(0).nodeValue
         self.id = entryNode.getElementsByTagName('id').item(0).childNodes.item(0).nodeValue
         self.dateAdded = entryNode.getElementsByTagName('published').item(0).childNodes.item(0).nodeValue
@@ -15,8 +20,8 @@ class Entry(object):
                 linkhref = n.getAttribute('href')
                 self.links[linkrel] = {'type': linktype, 'href': linkhref, 'rel': linkrel}
                 if linkrel == 'enclosure':
-                    self.links[linkrel].title = n.getAttribute('title')
-                    self.links[linkrel].size = n.getAttribute('size')
+                    self.links[linkrel]['title'] = n.getAttribute('title')
+                    self.links[linkrel]['size'] = n.getAttribute('size')
 
         contentNode = entryNode.getElementsByTagName("content").item(0)
         self.contentType = contentNode.getAttribute('type')
