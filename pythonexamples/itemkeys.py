ee348557e16e00a5fd223fb237cb74626d330f98
Faolan C-P <fcheslack@gmail.com>
2012-05-17 12:38:04 -0400

    string concat fix

--- pythonexamples/itemkeys.py
+++ pythonexamples/itemkeys.py
@@ -10,7 +10,7 @@ print(creds)
 zlib = zotero.Library(creds['libraryType'], creds['libraryID'], creds['librarySlug'], creds['apiKey'])
 
 itemKeys = zlib.fetchItemKeys()
-print("got " + len(itemKeys) + " item keys from Zotero api")
+print("got " + str(len(itemKeys)) + " item keys from Zotero api")
 
 for k in itemKeys:
     print k
