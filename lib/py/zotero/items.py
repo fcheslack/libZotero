7e8b6c6eedbbc1b05e364fd6080da20f01fb6ee0
Faolan C-P <fcheslack@gmail.com>
2012-05-22 16:14:04 -0400

    add sequence magic methods to items&collections
    Mostly just proxy to the relevant Dict magic methods.
    Nothing specific to Zotero objects

--- lib/py/zotero/items.py
+++ lib/py/zotero/items.py
@@ -6,9 +6,37 @@ class Items(object):
     def __init__(self):
         self.itemObjects = {}
 
+    def __len__(self):
+        return len(self.itemObjects)
+
+    def __getitem__(self, itemKey):
+        if itemKey in self.itemObjects:
+            return self.itemObjects[itemKey]
+        else:
+            raise KeyError
+
+    def __setitem__(self, itemKey, item):
+        self.itemObjects[itemKey] = item
+
+    def __delitem__(self, itemKey):
+        del self.itemObjects[itemKey]
+
+    def __iter__(self):
+        return self.itemObjects.__iter__()
+
+    def __reversed__(self):
+        ritems = Items()
+        ritems.itemObjects = self.itemObjects.reversed()
+        return ritems
+
+    def __contains__(self, itemKey):
+        return itemKey in self.itemObjects
+
     def getItem(self, itemKey):
         if itemKey in self.itemObjects:
             return self.itemObjects[itemKey]
+        else:
+            return None
 
     def addItem(self, item):
         itemKey = item.itemKey
--- lib/py/zotero/zcollections.py
+++ lib/py/zotero/zcollections.py
@@ -9,6 +9,32 @@ class Collections(object):
         self.dirty = False
         self.loaded = False
 
+    def __len__(self):
+        return len(self.collectionObjects)
+
+    def __getitem__(self, collectionKey):
+        if collectionKey in self.collectionObjects:
+            return self.collectionObjects[collectionKey]
+        else:
+            raise KeyError
+
+    def __setitem__(self, collectionKey, item):
+        self.collectionObjects[collectionKey] = item
+
+    def __delitem__(self, collectionKey):
+        del self.collectionObjects[collectionKey]
+
+    def __iter__(self):
+        return self.collectionObjects.__iter__()
+
+    def __reversed__(self):
+        ritems = Items()
+        ritems.collectionObjects = self.collectionObjects.reversed()
+        return ritems
+
+    def __contains__(self, collectionKey):
+        return collectionKey in self.collectionObjects
+
     def addCollection(self, collection):
         self.collectionObjects[collection.collectionKey] = collection
         self.orderedArray.append(collection)
