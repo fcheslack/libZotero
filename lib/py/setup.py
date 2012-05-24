de4cafb0e645db6f89e5f673a6fee75ed81618f5
Faolan C-P <fcheslack@gmail.com>
2012-05-24 18:33:41 -0400

    bump version for renamed package

--- lib/py/setup.py
+++ lib/py/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='libZotero',
-      version='0.1.0',
+      version='0.1.1',
       description='Python module for interacting with the zotero.org API',
       author='Faolan Cheslack-Postava',
       url='https://github.com/fcheslack/libZotero',
