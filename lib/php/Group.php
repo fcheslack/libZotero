9fa3a63a3048c3653c7e2496e79eedc634a8594c
Faolan C-P <fcheslack@gmail.com>
2012-08-14 15:06:50 -0400

    parse json groups

--- lib/php/Group.php
+++ lib/php/Group.php
@@ -115,23 +115,26 @@ class Zotero_Group extends Zotero_Entry
         if($contentType == 'application/json'){
             $this->apiObject = json_decode($contentNode->nodeValue, true);
             //$this->etag = $contentNode->getAttribute('etag');
+            $this->name = $this->apiObject['name'];
+            $this->ownerID = $this->apiObject['owner'];
+            $this->groupType = $this->apiObject['type'];
+            $this->description = $this->apiObject['description'];
+            $this->url = $this->apiObject['url'];
+            $this->libraryEnabled = $this->apiObject['libraryEnabled'];
+            $this->libraryEditing = $this->apiObject['libraryEditing'];
+            $this->libraryReading = $this->apiObject['libraryReading'];
+            $this->fileEditing = $this->apiObject['fileEditing'];
         }
-        
-        $this->name = $this->apiObject['name'];
-        $this->ownerID = $this->apiObject['owner'];
-        $this->groupType = $this->apiObject['type'];
-        $this->description = $this->apiObject['description'];
-        $this->url = $this->apiObject['url'];
-        $this->libraryEnabled = $this->apiObject['libraryEnabled'];
-        $this->libraryEditing = $this->apiObject['libraryEditing'];
-        $this->libraryReading = $this->apiObject['libraryReading'];
-        $this->fileEditing = $this->apiObject['fileEditing'];
         
         if(!empty($this->apiObject['admins'])){
             $this->adminIDs = $this->apiObject['admins'];
         }
         else {
             $this->adminIDs = array();
+        }
+        
+        if($this->ownerID){
+            $this->adminIDs[] = $this->ownerID;
         }
         
         if(!empty($this->apiObject['members'])){
@@ -165,20 +168,75 @@ class Zotero_Group extends Zotero_Entry
                 $this->adminIDs[] = $jsonObject['owner'];
                 $this->admins = $this->adminIDs;
             }
+            elseif($cNode->getAttribute('type') == 'application/xml'){
+                $groupElements = $entryNode->getElementsByTagName("group");
+                $groupElement = $groupElements->item(0);
+                if(!$groupElement) return;
+                
+                $groupAttributes = $groupElement->attributes;
+                $this->properties = array();
+                
+                foreach($groupAttributes as $attrName => $attrNode){
+                    $this->properties[$attrName] = urldecode($attrNode->value);
+                    if($attrName == 'name'){
+                        $this->$attrName = $attrNode->value;
+                    }
+                    else{
+                        $this->$attrName = urldecode($attrNode->value);
+                    }
+                }
+                $this->groupID = $this->properties['id'];
+                
+                $description = $entryNode->getElementsByTagName("description")->item(0);
+                if($description) {
+                    $this->properties['description'] = urldecode($description->nodeValue);
+                    $this->description = urldecode($description->nodeValue);
+                }
+                
+                $url = $entryNode->getElementsByTagName("url")->item(0);
+                if($url) {
+                    $this->properties['url'] = $url->nodeValue;
+                    $this->url = $url->nodeValue;
+                }
+                
+                $this->adminIDs = array();
+                $admins = $entryNode->getElementsByTagName("admins")->item(0);
+                if($admins){
+                    $this->adminIDs = $admins === null ? array() : explode(" ", $admins->nodeValue);
+                }
+                $this->adminIDs[] = $this->owner;
+                
+                $this->memberIDs = array();
+                $members = $entryNode->getElementsByTagName("members")->item(0);
+                if($members){
+                    $this->memberIDs = ($members === null ? array() : explode(" ", $members->nodeValue));
+                }
+                
+                //initially disallow library access
+                $this->userReadable = false;
+                $this->userEditable = false;
+            }
         }
         
-        //get link nodes and extract groupID
-        $linkNodes = $entryNode->getElementsByTagName("link");
-        if($linkNodes->length > 0){
-            for($i = 0; $i < $linkNodes->length; $i++){
-                $linkNode = $linkNodes->item($i);
-                if($linkNode->getAttribute('rel') == 'self'){
-                    $selfHref = $linkNode->getAttribute('href');
-                    $matches = array();
-                    preg_match('/^https:\/\/.{3,6}\.zotero\.org\/groups\/([0-9]+)$/', $selfHref, $matches);
-                    if(isset($matches[1])){
-                        $this->groupID = intval($matches[1]);
-                        $this->id = $this->groupID;
+        //get groupID from zapi:groupID if available
+        if($entryNode->getElementsByTagNameNS('*', 'groupID')->length > 0){
+            $this->groupID = $entryNode->getElementsByTagNameNS('*', 'groupID')->item(0)->nodeValue;
+            $this->id = $this->groupID;
+        }
+        else{
+            //get link nodes and extract groupID
+            $linkNodes = $entryNode->getElementsByTagName("link");
+            if($linkNodes->length > 0){
+                for($i = 0; $i < $linkNodes->length; $i++){
+                    $linkNode = $linkNodes->item($i);
+                    if($linkNode->getAttribute('rel') == 'self'){
+                        $selfHref = $linkNode->getAttribute('href');
+                        $matches = array();
+                        preg_match('/^https:\/\/.{3,6}\.zotero\.org\/groups\/([0-9]+)$/', $selfHref, $matches);
+                        if(isset($matches[1])){
+                            $this->groupID = intval($matches[1]);
+                            $this->id = $this->groupID;
+                        }
                     }
                 }
             }
@@ -197,11 +255,41 @@ class Zotero_Group extends Zotero_Entry
     
     public function updateString()
     {
-        $view = new Zend_View();
-        $view->setScriptPath('../library/Zotero/Service/Zotero/views');
-        $view->group = $this;
+        $doc = new DOMDocument();
+        $el = $doc->appendChild(new DOMElement('group'));
+        $el->appendChild(new DOMElement('description', urlencode($this->description)));
+        $el->appendChild(new DOMElement('url', $this->url));
+        if($this->groupID){
+            $el->setAttribute('id', $this->groupID);
+        }
+        $el->setAttribute('owner', $this->ownerID);
+        $el->setAttribute('type', $this->type);
+        $el->setAttribute('name', str_replace('&#039;', '&apos;', htmlspecialchars($this->name, ENT_QUOTES)));
+        $el->setAttribute('libraryEnabled', $this->libraryEnabled);
+        $el->setAttribute('libraryEditing', $this->libraryEditing);
+        $el->setAttribute('libraryReading', $this->libraryReading);
+        $el->setAttribute('fileEditing', $this->fileEditing);
+        $el->setAttribute('hasImage', $this->hasImage);
         
-        return $view->render("group.phtml");
+        return $doc->saveXML($el);
+    }
+    
+    public function propertiesArray()
+    {
+        $properties = array();
+        $properties['owner'] = $this->owner;
+        $properties['type'] = $this->type;
+        $properties['name'] = $this->name;
+        $properties['libraryEnabled'] = $this->libraryEnabled;
+        $properties['libraryEditing'] = $this->libraryEditing;
+        $properties['libraryReading'] = $this->libraryReading;
+        $properties['fileEditing'] = $this->fileEditing;
+        $properties['hasImage'] = $this->hasImage;
+        $properties['disciplines'] = $this->disciplines;
+        $properties['enableComments'] = $this->enableComments;
+        $properties['description'] = $this->description;
+        
+        return $properties;
     }
     
     public function dataObject() {
